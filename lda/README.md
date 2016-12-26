de68753f167bcb1d416206ff8ac14f48c0132118
Yongbo Wang <hust.wangyb@gmail.com>
2016-12-25 18:31:26 -0800

    lda

---
+++ lda/README.md
@@ -0,0 +1,9 @@
+# lda
+lda for document-topic-words model
+
+# usage
+git clone https://github.com/ybucla/lda.git
+
+cd lda
+
+Rscirpt lda.r
---
+++ lda/parseKS.r
@@ -0,0 +1,80 @@
+#! /u/home/y/ybwang/nobackup-yxing/program/R-3.2.3/bin/Rscript
+
+## check libraries
+if("doParallel" %in% rownames(installed.packages()) == FALSE) install.packages('doParallel')
+if("foreach" %in% rownames(installed.packages()) == FALSE) install.packages('foreach')
+
+## readubg data
+cat("# reading data\n")
+data <- read.table("data/parse.HL.out",header=FALSE,sep="\t")
+## remove kinase with less than 10 sites
+orikina <- unique(data$V7)
+for(i in 1:length(orikina)){
+  index <- which(data$V7 == orikina[i])
+  if(length(index) < 10) data <- data[-index,]
+}
+## generate corpus
+corpus <- NULL
+allsite <- paste0(data$V1,'_',data$V3,data$V2)
+site <- unique(allsite)
+kinase <- unique(data$V7)
+dataMatrix <- NULL
+for(i in 1:nrow(data)){
+  site_index <- which(site == allsite[i])
+  kina_index <- which(kinase == data$V7[i])
+  counts <- data$V5[i]
+  dataMatrix <- rbind(dataMatrix,c(kina_index,site_index,counts))
+}
+
+M <- length(kinase)
+for(i in 1:M){
+  index <- which(dataMatrix[,1]==i)
+  d <- dataMatrix[index,]
+  document <- matrix(numeric(nrow(d)*length(site)),nrow = nrow(d))
+  counts <- d[,2:3]
+  for(j in 1:nrow(d)){
+    document[j,d[j,2]] <- 1
+  }
+  corpus[[i]] <- list(document=document,counts=counts)
+}
+
+cat("# start runing\n")
+## start runing ##
+source("lda_func.r")
+core <- 6
+c1 <- makeCluster(core)
+registerDoParallel(c1)
+## parameters
+k <- 10
+v <- length(site)
+## simulate initial parameters
+sim_p <- sim_para(k,v)
+ini.alpha <- sim_p$alpha
+ini.beta <- sim_p$beta
+## inference alpha, beta
+alpha <- NULL
+beta <- NULL
+ll <- 0
+for(i in 1:5000){
+  if(is.null(alpha)) alpha <- ini.alpha
+  if(is.null(beta)) beta <- ini.beta
+  # e-step
+  pg <- e.step.parallel(corpus, alpha, beta, paral=TRUE)
+  var_gamma_list <- pg$g
+  phi_list <- pg$p
+  # m-step
+  abl <- m.step(corpus, var_gamma_list, phi_list, alpha, beta)
+  alpha <- abl$alpha
+  beta <- abl$beta
+  new_ll <- abl$ll
+  cat("# iter:",i,"\tll:",ll,"\tnew_ll:",new_ll,"\n")
+  if(abs(new_ll-ll) < 0.0001) break
+  ll <- new_ll
+}
+
+save(alpha,file="alpha.RData")
+save(beta,file="beta.RData")
+save(corpus,file="corpus.RData")
+
+## stopCluster
+stopCluster(cl)
