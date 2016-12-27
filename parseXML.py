ff6bb3bdbe1803ab96dfbb94cb5608d263a65d73
ybwang <hust.wangyb@gmail.com>
2016-12-27 22:21:40 +0800

    Update parseXML.py

--- parseXML.py
+++ parseXML.py
@@ -13,17 +13,27 @@ def main(id,xmlfile):
 	result = []
 	index = 1
 	Experiment = root.findall('Experiment')[0]
+
+	idmzdict = {}
+	spectrumList = Experiment.find('mzData').find('spectrumList')
+	for spectrum in spectrumList.findall('spectrum'):
+		sid = spectrum.get('id')
+		ionSelection = spectrum.find('spectrumDesc').find('precursorList').find('precursor').find('ionSelection')
+		for cvParam in ionSelection.findall('cvParam'):
+			if cvParam.get('name') == 'selected ion m/z':idmzdict[sid] = cvParam.get('value')
+
 	for TwoDimensionalIdentification in Experiment.findall('TwoDimensionalIdentification'):
 		Accession = TwoDimensionalIdentification.find('Accession').text
 		for PeptideItem in TwoDimensionalIdentification.findall('PeptideItem'):
 			Sequence = PeptideItem.find('Sequence').text
+			SpectrumReference = PeptideItem.find('SpectrumReference').text
 			SEQUEST_Score = ''
 			Delta_Cn = ''
 			additional = PeptideItem.find('additional')
 			for cvParam in additional.findall('cvParam'):
 				if cvParam.get('name') == 'SEQUEST Score': SEQUEST_Score = cvParam.get('value')
 				if cvParam.get('name') == 'Delta Cn': Delta_Cn = cvParam.get('value')
-			result.append(str(index)+'\t'+Sequence+'\t'+Accession+'\t'+SEQUEST_Score+';'+Delta_Cn)
+			result.append(str(index)+'\t'+Sequence+'\t'+Accession+'\t'+SEQUEST_Score+';'+Delta_Cn+'\t'+idmzdict[SpectrumReference])
 			index += 1
 	with open(outfile,'w') as fout:
 		for r in result:
