3260460578778f202e527143a136668b17a7a7d3
ybwang <hust.wangyb@gmail.com>
2017-01-23 08:30:59 +0800

    Update splitnfold.r

--- R/splitnfold/splitnfold.r
+++ R/splitnfold/splitnfold.r
@@ -12,3 +12,27 @@ for(i in 1:10){
     trainData <- yourData[-testIndexes, ]
     #Use the test and train data partitions however you desire...
 }
+
+
+# split positive and negative separetly
+# x: data, y: label
+pindex <- sample(which(y == 1))
+nindex <- sample(which(y == 0))
+
+pfolds <- cut(seq(1,length(pindex)),breaks=nfold,labels=FALSE)
+nfolds <- cut(seq(1,length(nindex)),breaks=nfold,labels=FALSE)
+  
+score <- numeric(length(y))
+for(i in 1:nfold){
+  ## Segement your data by fold using the which() function 
+  ptestIndexes <- pindex[which(pfolds==i,arr.ind=TRUE)]
+  ntestIndexes <- nindex[which(nfolds==i,arr.ind=TRUE)]
+  testIndexes <- c(ptestIndexes,ntestIndexes)
+  # test
+  xtest <- x[testIndexes, ]
+  ytest <- y[testIndexes]
+  # train
+  xtrain <- x[-testIndexes, ]
+  ytrain <- y[-testIndexes]
+  #Use the test and train data partitions however you desire...
+}
