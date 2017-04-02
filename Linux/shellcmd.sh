71ab6be5a1cb0652edf26c26f3f662ca77540c99
Yongbo <hust.wangyb@gmail.com>
2017-04-02 16:42:47 -0700

    Update shellcmd.sh

--- Linux/shellcmd.sh
+++ Linux/shellcmd.sh
@@ -30,5 +30,13 @@ split -d -l 3 input pre_
 ## vi 执行命令
 :!cmd
 
+## vi替换：
+:%s/vivian/sky/g (等同于 :g/vivian/s//sky/g) 替换每一行中所有 vivian 为 sky
+:s/vivian/sky/ 替换当前行第一个 vivian 为 sky
+:s/vivian/sky/g 替换当前行所有 vivian 为 sky
+:n,$s/vivian/sky/ 替换第 n 行开始到最后一行中每一行的第一个 vivian 为 sky
+:n,$s/vivian/sky/g 替换第 n 行开始到最后一行中每一行所有 vivian 为 sky (n 为数字，若 n 为 .，表示从当前行开始到最后一行)
+:%s/xxx//gn  关键是最后的n，代表只报告匹配的个数，而不进行实际的替换。
+
 ## less
 :&pattern - 仅显示匹配模式的行，而不是整个文件
