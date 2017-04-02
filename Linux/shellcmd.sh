816cb0931e31cb8b64231cd01a0a2adb1b2b00a5
Yongbo <hust.wangyb@gmail.com>
2017-04-02 16:41:40 -0700

    Update shellcmd.sh

--- Linux/shellcmd.sh
+++ Linux/shellcmd.sh
@@ -12,23 +12,23 @@ for i in {2..10};do printf $i'\t';done
 split -d -l 3 input pre_
 
 ## vi 设置书签
-ma			set mark a at current cursor location
-'a			jump to line of mark a (first non-blank character in line)
-`a			jump to position (line and column) of mark a
-d'a			delete from current line to line of mark a
-d`a			delete from current cursor position to position of mark a
-c'a			change text from current line to line of mark a
-y`a			yank text to unnamed buffer from cursor to position of mark a
+# ma			set mark a at current cursor location
+# 'a			jump to line of mark a (first non-blank character in line)
+# `a			jump to position (line and column) of mark a
+# d'a			delete from current line to line of mark a
+# d`a			delete from current cursor position to position of mark a
+# c'a			change text from current line to line of mark a
+# y`a			yank text to unnamed buffer from cursor to position of mark a
 :marks			list all the current marks
 :marks aB		list marks a, B
 :Ctrl + O: 上一个编辑位置
 
 ## vi 切换多个打开的文件 switch two files:
-# :n			Next file
-# :p			Previous file
+:n			Next file
+:p			Previous file
 
 ## vi 执行命令
 :!cmd
 
 ## less
-# :&pattern - 仅显示匹配模式的行，而不是整个文件
+:&pattern - 仅显示匹配模式的行，而不是整个文件
