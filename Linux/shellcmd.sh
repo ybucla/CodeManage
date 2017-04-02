7be7a3ed9f9cd6c8f7b4d41c07fd2bfef99cfd32
Yongbo <hust.wangyb@gmail.com>
2017-04-02 16:26:02 -0700

    Update shellcmd.sh

--- Linux/shellcmd.sh
+++ Linux/shellcmd.sh
@@ -19,5 +19,5 @@ d'a			delete from current line to line of mark a
 d`a			delete from current cursor position to position of mark a
 c'a			change text from current line to line of mark a
 y`a			yank text to unnamed buffer from cursor to position of mark a
-:marks		list all the current marks
-:marks aB	list marks a, B
+:marks			list all the current marks
+:marks aB			list marks a, B
